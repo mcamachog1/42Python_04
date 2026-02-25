c31ebf9625d47cd016572825b4e8932f0389903a
maryvi <maryvi.jobfinder@gmail.com>
2026-02-25 18:52:35 +0000

    ex0

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+def read_ancient_text(file_name: str) -> None:
+    try:
+        f = open(file_name, "r")
+    except FileNotFoundError:
+        print(f"Error: The file '{file_name}' is missing.")
+        return
+    except PermissionError:
+        print(
+            f"Error: The file '{file_name}' "
+            "does not have permission for reading.")
+        return
+    content = f.read()
+    f.close()
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    print(f"Accessing Storage Vault: {file_name}")
+    print("Connection established...\n")
+    print(content, end="")
+    print("\n\nData recovery complete. Storage unit disconnected.")
+
+
+def main() -> None:
+    file_name = "../test_files/ancient_fragment.txt"
+    # file_name = "../test_files/a.txt"
+    read_ancient_text(file_name)
+
+
+if __name__ == "__main__":
+    main()
